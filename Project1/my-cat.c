623db1325746711b0d8488c26e1192c414b2d711
Samuel Bunyan <sbunyan83@hotmail.com>
2018-09-18 23:11:13 +0000

    made some changes to my-cat

--- Project1/my-cat.c
+++ Project1/my-cat.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(int argc,char*argv[])
 {
-char str[999];
-FILE * file;
-file = fopen(argv[1],"r");
-if (file) {
-    while (fscanf(file, "%s", str)!=EOF)
-        printf("%s",str);
-    fclose(file);
+FILE * testFile;
+char str[255];
+testFile = fopen(argv[1],"r");
+if (testFile) {
+    while (fscanf(testFile, "%s", str)!=EOF)
+        printf("%s ",str);
+fclose(testFile);
 }}
 
