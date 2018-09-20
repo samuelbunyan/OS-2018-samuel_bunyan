8278ee717f5e71850b28e34288f2d8da9ee0054b
Samuel Bunyan <sbunyan83@hotmail.com>
2018-09-20 09:11:33 +0000

    Added exception clause

--- Project1/my-cat.c
+++ Project1/my-cat.c
@@ -4,9 +4,14 @@ int main(int argc,char*argv[])
 FILE * testFile;
 char str[255];
 testFile = fopen(argv[1],"r");
+
 if (testFile) {
     while (fscanf(testFile, "%s", str)!=EOF)
         printf("%s ",str);
+    }
+else if (testFile == NULL){
+	printf("There is no text in the file");
+	}
 fclose(testFile);
-}}
+}
 
