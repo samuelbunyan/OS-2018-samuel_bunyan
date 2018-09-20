0bd36e4f5d482a64a242169a844ec52d03e6e5ae
Samuel Bunyan <sbunyan83@hotmail.com>
2018-09-20 13:05:46 +0000

    minor changes

--- Project1/my-cat.c
+++ Project1/my-cat.c
@@ -1,18 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 int main(int argc,char*argv[])
 {
 FILE * testFile;
 char str[255];
+
 testFile = fopen(argv[1],"r");
 
-if (testFile) {
-    while (fscanf(testFile, "%s", str)!=EOF)
-        printf("%s ",str);
-    }
-else if (strstr(str,argv[1]) == NULL){
-	printf("There is no text in the file");
-	}
+if (testFile == NULL){
+	printf("cannot open file\n");
+	exit(1);
+}
+else if (testFile) {
+	while (fscanf(testFile, "%s", str)!=EOF)
+        	printf("%s ",str);
+}
 fclose(testFile);
 }
 
--- Project1/my-grep.c
+++ Project1/my-grep.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 int main(int argc, char*argv[])
@@ -6,10 +7,12 @@ FILE * testFile;
 char charCnstr[255];
 
 testFile = fopen(argv[2],"r");
+
 if(testFile){
         while(fscanf(testFile,"%[^\n]\n",charCnstr) !=EOF){
                 if(strstr(charCnstr,argv[1]) != NULL )
 			printf("%s \n",charCnstr);
 
-}fclose(testFile);
+}
+fclose(testFile);
 }}
